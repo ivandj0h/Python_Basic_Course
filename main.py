6e47a534667242186ef687be0a60f659b4debb6e
ivandjoh <ivandi.djoh@gmail.com>
2020-06-14 09:17:15 +0000

    created types of operators in Python

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 # Python Course
-# This branch focuses on two built-in functions print() and input() to perform I/O task in Python. Also, you will learn to import modules and use them in your program.
+# In this tutorial, you'll learn everything about different types of operators in Python, their syntax and how to use them with examples.
 
-# here is the example using format() :
-x = 5
-y = 10
-print('The value of x is {} and y is {}'.format(x,y))
+# Operators are special symbols in Python that carry out arithmetic or logical computation. The value that the operator operates on is called the operand.
+
+# Arithmetic operators
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
+
+# breakpoint
 print()
 
-# other example using format() :
-print('I love {0} and {1}'.format('bread','butter'))
-print('I love {1} and {0}'.format('bread','butter'))
-print()
+# Comparison operators
+x = 10
+y = 12
 
-# We can even use keyword arguments to format the string.
-print('Hello {name}, {greeting}'.format(greeting = 'Goodmorning', name = 'John'))
-print()
+# Output: x > y is False
+print('x > y is',x>y)
 
-# To allow flexibility, we might want to take the input from the user. In Python, we have the input() function to allow this. The syntax for input() is:
-num = input('Enter a number: ')
-print(num)
+# Output: x < y is True
+print('x < y is',x<y)
 
-# When our program grows bigger, it is a good idea to break it into different modules.
-import math
-print('this is pi number:', math.pi)
+# Output: x == y is False
+print('x == y is',x==y)
+
+# Output: x != y is True
+print('x != y is',x!=y)
+
+# Output: x >= y is False
+print('x >= y is',x>=y)
+
+# Output: x <= y is True
+print('x <= y is',x<=y)
