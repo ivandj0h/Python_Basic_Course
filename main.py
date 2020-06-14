c06be4a17958feb8b1c817c0daad42bc7b8a8e05
ivandjoh <ivandi.djoh@gmail.com>
2020-06-14 08:56:07 +0000

    i/o using format()

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # Python Course
+# This branch focuses on two built-in functions print() and input() to perform I/O task in Python. Also, you will learn to import modules and use them in your program.
 
-# here you can find topic by find it on every branch
-# such as variable, data type, etc
+# here is the example using format() :
+x = 5
+y = 10
+print('The value of x is {} and y is {}'.format(x,y))
+print()
 
-# i wrote this course using the simples way
+# other example using format() :
+print('I love {0} and {1}'.format('bread','butter'))
+print('I love {1} and {0}'.format('bread','butter'))
+print()
+
+# We can even use keyword arguments to format the string.
+print('Hello {name}, {greeting}'.format(greeting = 'Goodmorning', name = 'John'))
+print()
+
