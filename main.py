c6764c4c620641a443e0cd92f6dba3ac61b6ce6e
ivandjoh <ivandi.djoh@gmail.com>
2020-06-14 08:39:15 +0000

    in this branch we talk about data type

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-# variable
-# variable can be insert using many data type such
-# as "integer", "strings", "boolean", "dll"
+#Python Data Type
+# Every value in Python has a datatype. Since everything is an object in Python programming, data types are actually classes and variables are instance (object) of these classes.
+# Integers, floating point numbers and complex numbers fall under Python numbers category. They are defined as int, float and complex classes in Python.
+# We can use the type() function to know which class a variable or a value belongs to. Similarly, the isinstance() function is used to check if an object belongs to a particular class.
+a = 5
+print(a, "is of type", type(a))
 
-# integers example
-number = 1
-print('angka awal:',number)
+a = 2.0
+print(a, "is of type", type(a))
 
-# strings example
-my_str = 'ivandjoh'
-print('nama saya:',my_str)
-
-# concatenate more than two variables
-print(my_str, 'selalu menjadi nomor:', number)
-
-# variable can be use to calculate each other
-print(number,'+',number, '=', number + number)
-
-# we can use variable with any conditions
-if number > 0:
-  print(number, 'lebih besar dari 0')
-else:
-  print(number, 'lebih kecil dari 0')
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
