0175536b567a2c191c97dc8951bf65143ddff1ee
ivandjoh <ivandi.djoh@gmail.com>
2020-06-14 08:26:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# variable
+# variable can be insert using many data type such
+# as "integer", "strings", "boolean", "dll"
+
+# integers example
+number = 1
+print('angka awal:',number)
+
+# strings example
+my_str = 'ivandjoh'
+print('nama saya:',my_str)
+
+# concatenate more than two variables
+print(my_str, 'selalu menjadi nomor:', number)
+
+# variable can be use to calculate each other
+print(number,'+',number, '=', number + number)
+
+# we can use variable with any conditions
+if number > 0:
+  print(number, 'lebih besar dari 0')
+else:
+  print(number, 'lebih kecil dari 0')
