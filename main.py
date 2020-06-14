1cf1e909918ba5321c049f455da7cd986d450f2f
ivandjoh <ivandi.djoh@gmail.com>
2020-06-14 14:18:36 +0000

    Namespace and Scope

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 # Python Course
 
-# here you can find topic by find it on every branch
-# such as variable, data type, etc
+# Namespace
+# A namespace is a collection of names.
+# In Python, you can imagine a namespace as a mapping of every name you have defined to corresponding objects.
+# A namespace containing all the built-in names is created when we start the Python interpreter and exists as long as the interpreter runs.
 
-# i wrote this course using the simples way
+a = 2
+print('id(a) =', id(a))
 
-# i will update this Repo regularly
+a = a+1
+print('id(a) =', id(a))
 
-print("welcome to Python")
+print('id(3) =', id(3))
+
+b = 2
+print('id(b) =', id(b))
+print('id(2) =', id(2))
+
+# Variable Scope
+def outer_function():
+    a = 70
+
+    def inner_function():
+        a = 30
+        print('a =', a)
+
+    inner_function()
+    print('a =', a)
+
+
+a = 10
+outer_function()
+print('a =', a)
