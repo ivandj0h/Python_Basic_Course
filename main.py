a03ec833a07a0ca2cce315773686cbd8ce8fd97c
ivandjoh <ivandi.djoh@gmail.com>
2020-06-15 05:13:32 +0000

    added Employee class variable

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-#Python Data Type
-# Every value in Python has a datatype. Since everything is an object in Python programming, data types are actually classes and variables are instance (object) of these classes.
-# Integers, floating point numbers and complex numbers fall under Python numbers category. They are defined as int, float and complex classes in Python.
-# We can use the type() function to know which class a variable or a value belongs to. Similarly, the isinstance() function is used to check if an object belongs to a particular class.
-a = 5
-print(a, "is of type", type(a))
+#Python Class Variable
+# In this Python Object-Oriented Tutorial, we will be learning about class variables. We will see how they differ from instance variables and also some ideas for exactly how we would want to use them. Let's get started.
 
-a = 2.0
-print(a, "is of type", type(a))
+class Employee:
 
-a = 1+2j
-print(a, "is complex number?", isinstance(1+2j,complex))
+  def __init__(self, first_name, last_name, salary):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.salary = salary
+    self.email = first_name + '.' + last_name + '@company.com'
+
+  def full_name(self):
+    return '{} {}'.format(self.first_name, self.last_name)
+
+  def salary_rise(self):
+    self.salary = int(self.salary * 5)
+
+employee_one = Employee('ivandi', 'djoh', 2500)
+
+# print to the console
+print(employee_one.full_name() + ' salary is :')
+print(employee_one.salary)
+print(employee_one.full_name() + ' salary after added 5% :')
+employee_one.salary_rise()
+print(employee_one.salary)
